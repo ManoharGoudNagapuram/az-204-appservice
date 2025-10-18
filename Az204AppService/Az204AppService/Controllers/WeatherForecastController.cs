284dfe26b70cecb6591233a742e604476da29832
nagapurammanohar <nagapurammanohar@gmail.com>
2025-10-18 12:13:38 +0100

    modified

--- Az204AppService/Az204AppService/Controllers/WeatherForecastController.cs
+++ Az204AppService/Az204AppService/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace Az204AppService.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool1", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
